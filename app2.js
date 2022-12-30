1d84b24cbd63fb678b3140eaa9e60321901e9fa9
LeandroRapan <leandro.rapan@gmail.com>
2022-12-29 23:33:06 -0300

    trabajo incompleto

---
+++ app2.js
@@ -0,0 +1,59 @@
+// leandro rapan
+//Array con supuestos datos previos, en este caso el unicornio
+let arrListado=[{img: `imagenes\\MacetaProducto1.jpeg`, nombre: "unicornio", precio:"200"}];
+//funciones para crear objetos, hice dos creadorObjetos y creadorProductos, el cual refiere al primero; quizas sea redundante y con uno era suficiente
+function creadorObjetos (img, nombre, precio,  stock){
+    this.img = img;
+    this.nombre = nombre;
+    this.precio = precio;
+    
+    this.stock = stock;
+}
+
+function creadorProductos (){
+   let formulario = document.getElementById("formulario");
+   
+    
+  formulario.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    
+    let inputs = e.target.children;
+     let img = document.getElementById("imgSrc").value;
+    let nombre = document.getElementById("nombre").value;
+    let precio = document.getElementById("precio").value;
+    let stock = document.getElementById("stock").value;
+
+    let producto = new creadorObjetos (img, nombre, precio, stock);
+      arrListado.push(producto);
+       localStorage.setItem("ProductoStorange", JSON.stringify(producto));
+       escritorHtml();
+  })}  
+   
+  
+   creadorProductos()
+  function escritorHtml(){
+    
+    let productosStorange2 = JSON.parse(localStorage.getItem("productoStorange"));
+    
+      if (productosStorange2){arrListado = productosStorange2 } else {productosStorange2 = []}
+    
+     
+
+      productosStorange2.forEach( (producto)=> {
+        let contenedor = document.createElement("div");
+        contenedor.innerHTML= `<img src="${obj.img}">
+        <h2> ${producto.nombre}</h2>
+        <h2> $${producto.precio}      
+        `
+        contenedor.className="contenedor"
+     
+        document.body.appendChild(contenedor)
+    return arrListado
+    }
+        
+        )
+        
+    };
+   
+    escritorHtml()
+
